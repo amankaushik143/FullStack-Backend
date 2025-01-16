7937f85109b91441e42cc12d6bab08bdd8c5b5d6
Aman Sharma <amankaushik143@gmail.com>
2025-01-17 03:55:17 +0530

    Changed the port based on the  Frontend app
    Changed the port based on the  Frontend app

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS
+  app.enableCors({
+    origin: '*', // Allow all origins (for development only)
+    methods: 'GET',
+    credentials: true,
+  });
+
+  await app.listen(5000);
+  console.log('Application is running on: http://localhost:5000');
 }
 bootstrap();
